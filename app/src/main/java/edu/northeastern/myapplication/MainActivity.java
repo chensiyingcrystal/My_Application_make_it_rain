abbb28b2da626f45f21f9585a9ae6bd670511eb7
Siying Chen <chensiyingcrystal@gmail.com>
2023-01-19 12:20:03 +0800

    Initial commit

---
+++ app/src/main/java/edu/northeastern/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package edu.northeastern.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e("xat", "hello");
+    }
+}
